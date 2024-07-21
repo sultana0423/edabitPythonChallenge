c81554064fd6d7c1f0c061a95ac42fb8ccd17d59
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-21 22:40:19 +0300

    Create edge_of_triangle.py

---
+++ veryEasy/edge_of_triangle.py
@@ -0,0 +1,5 @@
+# Maximum Edge of a Triangle
+# Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+def next_edge(side1, side2):
+	return (side1 + side2) - 1
