1184b8defac2e58275bd93296bb2558c824d7462
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-21 22:27:27 +0300

    Create hours_into_seconds.py

---
+++ veryEasy/hours_into_seconds.py
@@ -0,0 +1,8 @@
+# instructions:
+# Convert Hours into Seconds
+# Write a function that converts hours into seconds.
+
+def how_many_seconds(hours):
+	minutes = hours * 60
+	seconds = minutes * 60
+	return seconds
