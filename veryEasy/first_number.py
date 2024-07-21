f799681a62050807a11aadffb02027842a127027
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-21 22:20:20 +0300

    Create first_number.py

---
+++ veryEasy/first_number.py
@@ -0,0 +1,7 @@
+# instructions:
+# Return the First Element in a List
+# Create a function that takes a list containing only numbers and return the first element.
+
+def get_first_value(number_list):
+	return number_list[0]
+	
