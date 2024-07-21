f5974db87f2aa8d73310793a50b462ac1d93da47
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-21 22:33:59 +0300

    Create next_num.py

---
+++ veryEasy/next_num.py
@@ -0,0 +1,6 @@
+# Return the Next Number from the Integer Passed
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+def addition(num):
+	return num + 1
+	
